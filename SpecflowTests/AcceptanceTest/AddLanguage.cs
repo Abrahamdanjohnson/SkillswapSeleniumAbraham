d5dfa2be1effaad912615c0dfef5c9a779cfd74b
Abrahamdanjohnson <47348110+Abrahamdanjohnson@users.noreply.github.com>
2019-03-03 13:41:28 +1300

    Add files via upload

---
+++ SpecflowTests/AcceptanceTest/AddLanguage.cs
@@ -0,0 +1,153 @@
+﻿using OpenQA.Selenium;
+using RelevantCodes.ExtentReports;
+using SpecflowPages;
+using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+using static SpecflowPages.CommonMethods;
+
+namespace SpecflowTests.AcceptanceTest
+{
+    [Binding]
+    public class SpecFlowFeature1Steps : Utils.Start
+    {
+        [Given(@"I launch browser and enter url")]
+        public void GivenILaunchBrowserAndEnterUrl()
+        {
+           // SpecflowPages.Driver.Initialize();
+          //  SpecflowPages.Utils.LoginPage.LoginStep();
+        }
+        [When(@"I entered the username, password and click on login button")]
+        public void WhenIEnteredTheUsernamePasswordAndClickOnLoginButton()
+        {
+        }
+
+        [Then(@"My profile home page should be displayed")]
+        public void ThenMyProfileHomePageShouldBeDisplayed()
+        {
+            SpecflowPages.Utils.LoginPage.VerifyHomePage();
+        }
+        [Given(@"Click on languages and then click on “Add new”")]
+        public void GivenClickOnLanguagesAndThenClickOnAddNew()
+        {
+            SpecflowPages.Utils.LanguagesPage.AddNewLanguage();
+
+        }
+        [When(@"I entered the Language, level and clicked on “Add” button")]
+        public void WhenIEnteredTheLanguageLevelAndClickedOnAddButton()
+        {
+            SpecflowPages.Utils.LanguagesPage.AddLanguage();
+        }
+
+        [Then(@"The language is displayed")]
+        public void ThenTheLanguageIsDisplayed()
+        {
+            SpecflowPages.Utils.LanguagesPage.VerifyLanguage();
+        }
+
+        [Given(@"I navigate to Profile and click Skills Tab and Click on edit button")]
+        public void GivenINavigateToProfileAndClickSkillsTabAndClickOnEditButton()
+        {
+            SpecflowPages.Utils.SkillsPage.ClickEditSkill();
+        }
+        [Given(@"Edit the Skill text box")]
+        public void GivenEditTheSkillTextBox()
+        {
+            SpecflowPages.Utils.SkillsPage.EditSkill();
+        }
+        [When(@"I click Update button")]
+        public void WhenIClickUpdateButton()
+        {
+            SpecflowPages.Utils.SkillsPage.ClickUpdate();
+        }
+        [Then(@"The skill is updated")]
+        public void ThenTheSkillIsUpdated()
+        {
+            Thread.Sleep(1000);
+            SpecflowPages.Utils.SkillsPage.VerifySkillsUpdated();
+        }
+       
+        [Given(@"I navigate to Profile and click Languages Tab")]
+        public void GivenINavigateToProfileAndClickLanguagesTab()
+        {
+            SpecflowPages.Utils.LanguagesPage.ClickTab();
+
+        }
+
+        [When(@"I click remove button")]
+        public void WhenIClickRemoveButton()
+        {
+            SpecflowPages.Utils.LanguagesPage.RemoveLanguage();
+        }
+
+        [Then(@"The language is deleted")]
+        public void ThenTheLanguageIsDeleted()
+        {
+            SpecflowPages.Utils.LanguagesPage.Removed();
+        }
+
+        /*[Given(@"I clicked on the Language tab under Profile page")]
+        public void GivenIClickedOnTheLanguageTabUnderProfilePage()
+        {
+            //Wait
+            Thread.Sleep(1500);
+       
+            // Click on Profile tab
+            Driver.driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[1]/div/a[2]")).Click();
+
+            
+        }
+        
+        [When(@"I add a new language")]
+        public void WhenIAddANewLanguage()
+        {
+            //Click on Add New button
+            Driver.dri ver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/thead/tr/th[3]/div")).Click();
+
+            //Add Language
+            Driver.driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/div/div[1]/input")).SendKeys("English");
+
+            //Click on Language Level
+            Driver.driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/div/div[2]/select")).Click();
+
+            //Choose the language level
+            IWebElement Lang = Driver.driver.FindElements(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/div/div[2]/select/option"))[1];
+            Lang.Click();
+
+            //Click on Add button
+            Driver.driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/div/div[3]/input[1]")).Click();
+
+        }
+
+        [Then(@"that language should be displayed on my listings")]
+        public void ThenThatLanguageShouldBeDisplayedOnMyListings()
+        {
+            try
+            {
+                //Start the Reports
+                CommonMethods.ExtentReports();
+                Thread.Sleep(1000);
+                CommonMethods.test = CommonMethods.extent.StartTest("Add a Language");
+
+                Thread.Sleep(1000);
+                string ExpectedValue = "English";
+                string ActualValue = Driver.driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody/tr/td[1]")).Text;
+                Thread.Sleep(500);
+                if(ExpectedValue == ActualValue)
+                {
+                    CommonMethods.test.Log(LogStatus.Pass, "Test Passed, Added a Language Successfully");
+                    SaveScreenShotClass.SaveScreenshot(Driver.driver, "LanguageAdded");
+                }
+
+                else
+                    CommonMethods.test.Log(LogStatus.Fail, "Test Failed");
+
+            }
+            catch(Exception e)
+            {
+                CommonMethods.test.Log(LogStatus.Fail, "Test Failed",e.Message);
+            }
+
+        }*/
+    }
+}
---
+++ SpecflowTests/AcceptanceTest/SpecFlowFeature1.feature
@@ -0,0 +1,126 @@
+﻿Feature: SpecFlowFeature1
+	In order to update my profile 
+	As a skill trader
+	I want to add the languages that I know
+
+@mytag
+Scenario: Check if user could able to add a language 
+	Given I clicked on the Language tab under Profile page
+	When I add a new language
+	Then that language should be displayed on my listings
+
+Scenario: Check if user is able to login to the website
+Given I entered the website name www.Skillswap.pro
+When  I Click enter button
+Then  The website is displayed
+
+Scenario:Check if user is able to join as member in www.Skillswap.pro
+Given I clicked on the Join tab
+When I entered the fields for First name, Last name, Email address, password, confirm password and click on agree to terms and condition, then click the join button
+Then : The user profile is created
+
+Scenario:Check if the user is able to login with the Username and password
+Given I launch browser and enter url
+When I entered the username, password and click on login button
+Then  My profile home page should be displayed
+
+Scenario:Check if user is able to edit the name in the profile
+Given click on edit name
+When I entered my name and clicked on save button
+Then My name is displayed on the profile section 
+
+@Test1
+Scenario:Check if user is able to add Languages in profile section
+Given Click on languages and then click on “Add new”
+When I entered the Language, level and clicked on “Add” button
+Then The language is displayed
+
+Scenario:Check if user is able to add skills 
+Given Click on skills and then click on “Add new”
+When I entered the skill, level and clicked on “Add” button
+Then The skills is displayed 
+
+Scenario:Check if user is able to add education
+Given Click on education  and click on “Add new”
+When I entered the College name, Selected the country of college, enter title, Degree and select the year of graduation and clicked on “Add” button
+Then The education is displayed
+
+Scenario:Check if user is able to add Certification 
+Given Click on certification and click on “Add new”
+When I entered the name of certificate, certificate from and selected the year and clicked on “Add” button
+Then The certification is displayed 
+
+Scenario:Check if user is able to edit Languages
+ Given I navigate to Profile and on Languages Tab Click on edit button 
+And Edit the Language text box 
+When I click Update button 
+Then The language is updated 
+
+@Test2
+Scenario:Check if user is able to edit Skills
+ Given I navigate to Profile and click Skills Tab and Click on edit button 
+And Edit the Skill text box 
+When I click Update button 
+Then The skill is updated 
+
+Scenario:Check if user is able to edit Education
+ Given I navigate to Profile and click Education Tab and Click on edit button 
+And Edit the Year in the Year dropdown 
+When I click Update button 
+Then The Education is updated 
+
+Scenario:Check if user is able to edit Certifications
+ Given I navigate to Profile and click Certifications Tab and Click on edit button 
+And Edit the From textbox 
+When I click Update button 
+Then The Certification is updated 
+
+Scenario:Check if user is able to edit and cancel on Education Tab
+ Given I navigate to Profile and click Education Tab and Click on edit button 
+When I click Cancel button 
+Then The Education tab is displayed  
+
+@Test3
+Scenario:Check if user is able to delete Language
+ Given I navigate to Profile and click Languages Tab 
+When I click remove button 
+Then The language is deleted 
+
+Scenario:Check if user is able to delete Skills
+ Given I navigate to Profile and click Skills Tab 
+When I click remove button 
+Then The skill is deleted 
+
+Scenario:Check if user is able to delete Education
+ Given I navigate to Profile and click Education Tab 
+When I click remove button 
+Then The education is deleted 
+
+Scenario:Check if user is able to delete Certification
+ Given I navigate to Profile and click Certifications Tab 
+When I click remove button 
+Then The certification is deleted 
+
+Scenario:Check if user is able to edit and cancel on Languages Tab
+ Given I navigate to Profile and click Languages Tab 
+And : Click on edit button
+When I click Cancel button 
+Then The Languages tab is displayed  
+
+Scenario:Check if user is able to edit and cancel on Skills Tab
+ Given I navigate to Profile and click Skills Tab 
+And : Click on edit button
+When I click Cancel button 
+Then The Skills tab is displayed  
+
+Scenario:Check if user is able to edit and cancel on Certifications Tab
+ Given Click Certifications Tab
+And : Click on edit button
+When I click Cancel button 
+Then The Certification tab is displayed  
+
+Scenario:Check if user is able to edit and Save description
+ Given Click edit button on Description Tab 
+When I enter details 
+And Click Save button
+Then The Description is added
